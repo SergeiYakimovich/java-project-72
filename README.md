77f22c3581ac97463925c549093e30895755b11f
sergei <s0108@mail.ru>
2022-09-14 15:59:34 +0300

    Add badges

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SergeiYakimovich/java-project-72/workflows/hexlet-check/badge.svg)](https://github.com/SergeiYakimovich/java-project-72/actions)
+
+![Java CI](https://github.com/SergeiYakimovich/java-project-72/workflows/Java%20CI/badge.svg)
+[![Maintainability](https://api.codeclimate.com/v1/badges/f3ee4e3c13694206eb0b/maintainability)](https://codeclimate.com/github/SergeiYakimovich/java-project-72/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/f3ee4e3c13694206eb0b/test_coverage)](https://codeclimate.com/github/SergeiYakimovich/java-project-72/test_coverage)
+
+### App on Heroku:
+https://stark-bayou-06841.herokuapp.com/
--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -15,41 +15,33 @@ public final class UrlController {
     public static Handler listUrls = ctx -> {
         int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
         int rowsPerPage = 10;
-
         PagedList<Url> pagedUrls = new QUrl()
                 .setFirstRow(page * rowsPerPage)
                 .setMaxRows(rowsPerPage)
                 .orderBy()
                 .id.asc()
                 .findPagedList();
-
         List<Url> urls = pagedUrls.getList();
-
         int lastPage = pagedUrls.getTotalPageCount() + 1;
         int currentPage = pagedUrls.getPageIndex() + 1;
         List<Integer> pages = IntStream
                 .range(1, lastPage)
                 .boxed()
                 .collect(Collectors.toList());
-
         ctx.attribute("urls", urls);
         ctx.attribute("pages", pages);
         ctx.attribute("currentPage", currentPage);
-
         ctx.render("listUrls.html");
     };
 
     public static Handler showUrl = ctx -> {
         int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
-
         Url url = new QUrl()
                 .id.equalTo(id)
                 .findOne();
-
         if (url == null) {
             throw new NotFoundResponse();
         }
-
         ctx.attribute("url", url);
         ctx.render("showUrl.html");
     };
